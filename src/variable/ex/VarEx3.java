d586416fdc12fe6d8c3672420cca7f4ed9edfa49
ehdnd_desktop <taeungjang@naver.com>
2024-12-25 21:40:20 +0900

    섹션3 변수

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVal = 10000000000L;
+        boolean booleanVal = true;
+        System.out.println(longVal);
+        System.out.println(booleanVal);
+    }
+}
