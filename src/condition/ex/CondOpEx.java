063a46996d6913878706a596c955b349bdb04c18
ehdnd_desktop <taeungjang@naver.com>
2024-12-26 20:42:05 +0900

    섹션5 조건문

---
+++ src/condition/ex/CondOpEx.java
@@ -0,0 +1,8 @@
+package condition.ex;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(a > b ? a : b);
+    }
+}
---
+++ src/condition/ex/EvenOddEx.java
@@ -0,0 +1,8 @@
+package condition.ex;
+
+public class EvenOddEx {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x % 2 == 0 ? "짝수" : "홀수");
+    }
+}
